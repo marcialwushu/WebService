9fbcedecfa1626bebcd614914b70bf38f8df789a
cleilson pereira <marcialwushu@hotmail.com>
2023-01-29 16:47:59 -0300

    Add project files.

---
+++ ConsoleAppWSDL/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppWSDL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ ConsoleAppWebService/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using ServiceReference1;
+using static ServiceReference1.CountryInfoServiceSoapTypeClient;
+
+namespace ConsoleAppWebService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            consumeWebServiceCountry("DO");
+        }
+
+        private static void consumeWebServiceCountry(string code)
+        {
+            CountryInfoServiceSoapTypeClient client = new CountryInfoServiceSoapTypeClient(EndpointConfiguration.CountryInfoServiceSoap);
+            CapitalCityRequestBody cityRequestBody = new CapitalCityRequestBody(code);
+            CapitalCityRequest cityRequest = new CapitalCityRequest(cityRequestBody);
+            CapitalCityResponse cityResponse = client.CapitalCity(cityRequest);
+
+            Console.WriteLine(cityResponse.Body.CapitalCityResult);
+        }
+    }
+}
